ae604d497ee865f390a101a318435fcf55f7f400
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-10 05:08:05 -0800

    add constructor to Unit Class

--- main.js
+++ main.js
@@ -4,13 +4,17 @@
 
 //  //// CARD CLASS ////////////////////////////////
 class Card {
-    //  **** CONSTRUCTORS **************************
+    //  **** CONSTRUCTOR ***************************
     constructor (name, cost) {
         this.name = name;       // Name of Card
         this.cost = cost;       // Cost of Card to Play
     }
 }
 
+//  //// UNIT CLASS ////////////////////////////////
 class Unit extends Card {
-
+    //  **** CONSTRUCTOR ***************************
+    constructor (name, cost, power, res) {
+        
+    }
 }
